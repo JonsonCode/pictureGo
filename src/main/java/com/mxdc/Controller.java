3ef1ba6260ab7961f827ee931dd7e0ed24781272
mxdc <1040558191@qq.com>
2019-07-19 18:11:01 +0800

    edti pom add JFoeniX

--- src/main/java/com/mxdc/Controller.java
+++ src/main/java/com/mxdc/Controller.java
@@ -1,4 +1,32 @@
 package com.mxdc;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ *  @author  mxdc
+ *  @date    2019/7/19 17:46
+ */
+public class Controller implements Initializable {
+    @FXML
+    private Button myButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void buttonClick(ActionEvent event){
+        System.out.println("sdf");
+    }
+
+
+    public void button(javafx.event.ActionEvent actionEvent) {
+        System.out.println("sdff");
+    }
 }
