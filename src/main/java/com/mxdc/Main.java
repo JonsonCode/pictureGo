c84d6f464b3484ed6f1df44af4603fe3b520057a
lollipop <1769128867@qq.com>
2019-07-23 22:58:52 +0800

    modify Main.java

--- src/main/java/com/mxdc/Main.java
+++ src/main/java/com/mxdc/Main.java
@@ -1,35 +1,29 @@
 package com.mxdc;
 
-import com.jfoenix.assets.JFoenixResources;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root =
-                FXMLLoader.load(getClass().getResource("/main.fxml"));
-        // 设置标题
-        primaryStage.setTitle("Picture Bed");
-        //设置图标
-        primaryStage.getIcons().add(new Image("/images/tu.png"));
-        Scene scene = new Scene(root, 650, 458);
-        scene.getStylesheets().addAll(
-                JFoenixResources.load("css/jfoenix-fonts.css").toExternalForm(),
-                JFoenixResources.load("css/jfoenix-design.css").toExternalForm()
-        );
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Picture Bed"); // 设置标题
+        primaryStage.getIcons().add(new Image("/image/ApplicationIcon.png")); //设置图标
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED); //去掉默认的标题栏
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
